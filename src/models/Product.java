f9fb01738950c1a0c3cae8aa357c70ab8e0adaa2
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-07-04 17:01:20 +0300

    Add constructor overloading for different product types

--- src/models/Product.java
+++ src/models/Product.java
@@ -1,13 +1,31 @@
 package models;
 
+import behaviors.*;
+
 public class Product {
     private final String name;
     private final double price;
     int quantity;
-    Product(String name, double price, int quantity) {
+    private final Shippable shippable;
+    private final Expirable expirable;
+    public Product(String name, double price, int quantity) {
+        this(name, price, quantity, null, null);
+    }
+
+    public Product(String name, double price, int quantity, Shippable shippable) {
+        this(name, price, quantity, shippable, null);
+    }
+
+    public Product(String name, double price, int quantity, Expirable expirable) {
+        this(name, price, quantity, null, expirable);
+    }
+
+    public Product(String name, double price, int quantity, Shippable shippable, Expirable expirable) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.shippable = shippable;
+        this.expirable = expirable;
     }
     void decreaseQuantity(int amount) {
         quantity -= amount;
