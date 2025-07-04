66eee7ac5d8062b573e427ab1e5e9c63ba9ebd1b
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-07-04 16:29:00 +0300

    Add behaviours for composition logic

---
+++ src/behaviors/Expirable.java
@@ -0,0 +1,5 @@
+package behaviors;
+
+public interface Expirable {
+    boolean isExpired();
+}
---
+++ src/behaviors/Shippable.java
@@ -0,0 +1,6 @@
+package behaviors;
+
+public interface Shippable {
+    String getName();
+    double getWeight();
+}
