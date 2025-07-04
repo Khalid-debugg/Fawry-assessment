3af592728a8ed020a632cc825a88b375b32c1ebf
Khalid-debugg <khalidsherif.dev@gmail.com>
2025-07-04 19:47:27 +0300

    Add test input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import models.*;
+import behaviors.*;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+        Product cheese = new Product("Cheese", 100, 5, new CanBeShipped("Cheese", 200), new CanExpire(LocalDate.of(2025, 10    , 1)));
+        Product biscuits = new Product("Biscuits", 150, 2, new CanBeShipped("Biscuits", 700));
+        Customer customer = new Customer("Ali", 10000);
+        Cart cart1 = new Cart();
+        cart1.add(cheese, 2);
+        cart1.add(biscuits, 1);
+        cart1.checkout(customer);
     }
 }
